dfa7511cde7839b60b0a23647fdea724d92a32f7
Brian Lara <brian.lara@outlook.com.ar>
2024-04-19 09:36:52 -0300

    TP03EJ14

---
+++ Semana 05/ej14tp3_b.cpp
@@ -0,0 +1,44 @@
+///Ejercicio:
+///Autor:DEK
+///Fecha:
+///Comentario:
+
+# include<iostream>
+# include<cstdlib>
+
+using namespace std;
+
+
+int main(){
+    int i, num, primerImpar=2, ultimoImpar=2, segundoImpar=2, anteUltimoImpar=2;
+    int cuentaImpares=0;
+    for(i=1;i<=7;i++){
+        ///se repite 7 veces
+        cout<<"NUMERO ";
+        cin>>num;
+        if(num%2!=0){
+            ///pasan solo los impares
+            cuentaImpares++;
+            if(cuentaImpares==1){
+                primerImpar=num;
+            }
+            if(cuentaImpares==2){
+                    segundoImpar=num;
+            }
+            anteUltimoImpar=ultimoImpar;
+            ultimoImpar=num;
+        }
+    }
+	cout<<endl;
+	if(cuentaImpares==0){
+        cout<<"NO HUBO IMPARES"<<endl;
+	}
+	else{
+        if(primerImpar!=2) cout<<"EL PRIMER IMPAR ES "<<primerImpar<<endl;
+        if(segundoImpar!=2) cout<<"EL SEGUNDO IMPAR ES "<<segundoImpar<<endl;
+        if(anteUltimoImpar!=2) cout<<"EL ANTEULTIMO IMPAR ES "<<anteUltimoImpar<<endl;
+        if(ultimoImpar!=2) cout<<"EL ULTIMO IMPAR ES "<<ultimoImpar<<endl;
+	}
+	system("pause");
+	return 0;
+}
